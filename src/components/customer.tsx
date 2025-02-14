a085ff78211b634491f66fb39d4d5523ee494f3b
TINUNGTAA <tinungtaa@gmail.com>
2025-02-14 22:31:18 +0000

    updated features

--- src/components/customer.tsx
+++ src/components/customer.tsx
@@ -1,7 +1,7 @@
 function Customer() {
   return (
     <section className="  ">
-      <div className="mt-16  w-6/12 lg:w-8/12 bg-useBlue lg:py-12 mx-auto py-3 lg:py-6 px:6 lg:px-12 rounded-xl text-white  bg-gradient-to-b from-blue-300 to-blue-600 ">
+      <div className="mt-16  w-8/12 bg-useBlue lg:py-12 mx-auto  py-6 px:6 lg:px-12 rounded-xl text-white  bg-gradient-to-b from-blue-300 to-blue-600 ">
         <h1 className="text-center mt-6 text-xl lg:text-3xl font-semibold mx-auto lg:leading-loose">
           What our customer are saying
           <div className="hidden lg:flex w-16 h-1 bg-white mx-auto rounded-full my-3"></div>
